45ea9a9391b7e7cd39829a019a9110c941ce18dd
Valentin-Funaro <valentinfunaro3@gmail.com>
2025-05-14 16:40:06 -0300

    Hola Mundo

---
+++ src/main/java/poo/Paleta.java
@@ -0,0 +1,23 @@
+package poo;
+
+public class Paleta extends ObjetoMovible {
+
+    public Paleta(int x, int y) {
+        super(x, y);
+    }
+
+    @Override
+    public void dibujar() {
+
+    }
+
+    @Override
+    public void mover() {
+
+    }
+
+    @Override
+    public boolean detectarColision() {
+        return false;
+    }
+}
